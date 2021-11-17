d51efa11623d901d524c234e7aa98204db5324a7
TaherJerbi <jerbi.taherr@gmail.com>
2021-11-17 12:16:16 +0100

    breaking linter

--- src/app/store.js
+++ src/app/store.js
@@ -1,36 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "@reduxjs/toolkit";
-import { createBrowserHistory } from "history";
-import { createRouterReducer } from "@lagunovsky/redux-react-router";
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
-export const history = createBrowserHistory();
+import { createBrowserHistory } from 'history'
+import { createRouterReducer } from '@lagunovsky/redux-react-router'
+
+export const history = createBrowserHistory()
 
 const configureInjectableStore = (setup) => {
-  const store = configureStore(setup);
+  const store = configureStore(setup)
 
-  store.asyncReducers = {};
+  store.asyncReducers = {}
 
   store.injectReducer = (key, asyncReducer) => {
-    if (typeof asyncReducer !== "function" || (store.asyncReducers[key] && store.asyncReducers[key] === asyncReducer))
-      return;
-    store.asyncReducers[key] = asyncReducer;
-    store.replaceReducer(createReducer(store.asyncReducers));
-  };
-
-  function createReducer(asyncReducers) {
-    return combineReducers({
-      router: createRouterReducer(history),
-      ...asyncReducers,
-    });
+    if (typeof asyncReducer !== 'function' || (store.asyncReducers[key] && store.asyncReducers[key] === asyncReducer)) { return }
+    store.asyncReducers[key] = asyncReducer
+    store.replaceReducer(createReducer(store.asyncReducers))
   }
 
-  return store;
-};
+  function createReducer (asyncReducers) {
+    return combineReducers({
+      router: createRouterReducer(history),
+      ...asyncReducers
+    })
+  }
+
+  return store
+}
 const createRootReducer = (history) =>
   combineReducers({
-    router: createRouterReducer(history),
-  });
+    router: createRouterReducer(history)
+  })
 export const store = configureInjectableStore({
   reducer: createRootReducer(history),
-  devTools: true,
-});
+  devTools: true
+})
