f5b8c0f88d7f06fc28da530029b1e4f14f0fb611
TaherJerbi <jerbi.taherr@gmail.com>
2021-11-17 15:05:44 +0100

    testing pre-commit

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+// import PropTypes from 'prop-types'
+
+function Todo () {
+  return (
+  <div>
+    Todo
+  </div>
+  )
+}
+
+// Todo.propTypes = {}
+
+export default memo(Todo)
