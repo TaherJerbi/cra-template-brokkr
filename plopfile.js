034bcd5345af0f9df618d24fdaac09c8f7dfbd47
TaherJerbi <jerbi.taherr@gmail.com>
2021-11-17 14:46:39 +0100

    page generator

--- plopfile.js
+++ plopfile.js
@@ -1,47 +1,69 @@
+const componentPrompts = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What do you want to name your component?'
+  },
+  {
+    type: 'confirm',
+    name: 'memo',
+    message: 'Do you want wrap this component in React.memo?'
+  }
+]
+const componentActions = (path) => [
+  {
+    type: 'add',
+    path: `src/${path}/{{properCase name}}/{{properCase name}}.jsx`,
+    templateFile: 'internals/generators/templates/Component/Component.jsx.hbs'
+  }
+]
+const containerPrompts = [
+  ...componentPrompts,
+  {
+    type: 'input',
+    name: 'sliceName',
+    message: 'Which slice do you want to connect to this container ?'
+  }
+]
+const containerActions = (path) => [
+  ...componentActions(path),
+  {
+    type: 'add',
+    path: `src/${path}/{{properCase name}}/index.js`,
+    templateFile: 'internals/generators/templates/Container/index.js.hbs'
+  }
+]
+const pagePrompts = [
+  ...containerPrompts
+]
+const pageActions = (path) => [
+  ...componentActions(path),
+  {
+    type: 'add',
+    path: `src/${path}/{{properCase name}}/index.js`,
+    templateFile: 'internals/generators/templates/Page/index.js.hbs'
+  }
+]
 const config = plop => {
   plop.setGenerator('Component', {
     description: 'Generate a Presentational React Component',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What do you want to name your component?'
-      }
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/components/{{name}}/{{name}}.jsx',
-        templateFile: 'internals/generators/templates/Component.jsx.hbs'
-      }
-    ]
+    prompts: componentPrompts,
+    actions: componentActions('components')
   })
   plop.setGenerator('Container', {
     description: 'Generate a Container React Component',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What do you want to name your component?'
-      },
-      {
-        type: 'input',
-        name: 'sliceName',
-        message: 'Which slice do you want to connect to this container ?'
-      }
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/components/{{name}}/{{name}}.jsx',
-        templateFile: 'internals/generators/templates/Component/Component.jsx.hbs'
-      },
-      {
-        type: 'add',
-        path: 'src/components/{{name}}/index.js',
-        templateFile: 'internals/generators/templates/Container/index.js.hbs'
-      }
-    ]
+    prompts: containerPrompts,
+    actions: containerActions('components')
+  })
+  plop.setGenerator('Container', {
+    description: 'Generate a Container React Component',
+    prompts: containerPrompts,
+    actions: containerActions('components')
+  })
+  plop.setGenerator('Page', {
+    description: 'Generate a Page',
+    prompts: pagePrompts,
+    actions: pageActions('pages')
   })
   plop.setGenerator('Feature', {
     description: 'Generate a redux Slice inside the features folder',
